b0651fe4da434adf1ebf270301d49347ac69a28f
oleksashcherbakov <oleksa.shcherbakov@gmail.com>
2024-10-02 21:56:45 +0300

    'Solution'

---
+++ musician/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from musician.models import Musician
+
+
+class MusicianSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Musician
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "is_adult",
+            "date_of_applying",
+        )
